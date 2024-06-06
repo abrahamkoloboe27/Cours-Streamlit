42e721bf258f4afaa2737919ecee3979eb592146
Abraham KOLOBOE <114146395+abrahamkoloboe27@users.noreply.github.com>
2024-06-06 12:15:28 +0100

    Update 2_🚼_Fondamentaux.py

--- pages/2_🚼_Fondamentaux.py
+++ pages/2_🚼_Fondamentaux.py
@@ -12,12 +12,15 @@ with st.sidebar :
     ## Auteurs
     John R. AOGA
     * Linkedin : [John AOGA](https://www.linkedin.com/in/john-aoga-2ba6a862/)
-                
-    Abraham KOLOBOE
-    * Email : <abklb27@gmail.com>
-    * WhatsApp : +229 91 83 84 21
-    * Linkedin : [Abraham KOLOBOE](https://www.linkedin.com/in/abraham-koloboe-630683263)
-                """)
+""")
+with st.sidebar : 
+        st.markdown("""
+        ## Auteur
+        :blue[Abraham KOLOBOE]
+        * Email : <abklb27@gmail.com>
+        * WhatsApp : +229 91 83 84 21
+        * Linkedin : [Abraham KOLOBOE](https://www.linkedin.com/in/abraham-zacharie-koloboe-data-science-ia-generative-llms-machine-learning)
+                    """)
 st.title("🚼 Fondamentaux")
 tab1,tab2,tab3,tab4 = st.tabs(["👋🏾Hello World !","🔤Texte",
                                "🖇️Widget","🎞️Layouts" ])
